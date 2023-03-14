40c35b14acc051fda8dde14408692fb41c1829ee
MartinaDR1 <derosemartina933@gmail.com>
2023-03-14 15:56:30 +0100

    add btn next

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-let img = document.createElement("img"); 
-  
 const images = [
     './assets/img/01.webp',
     './assets/img/02.webp',
@@ -14,11 +12,36 @@ let activeImage = 0;
 
 const imagesElement = document.querySelector (`.slider > .images`);
 
-for (let i = 0; i<images.length; i++){
+for (let i = 0; i < images.length; i++){
     const imgArrayEl= images[i];
     const imgElement = `<img class="img-fluid ${i === activeImage ? 'active' : ''}" src="${imgArrayEl}" alt="">`
-    console.log(imgElement);
+    
 
     //Inserisco all'interno della DOM
     imagesElement.innerHTML += imgElement
 }
+
+// Seleziono tutte le immagini
+const allImages = document.querySelectorAll ('.slider > .images > img');
+
+// Seleziono il bottone
+const btnNext = document.querySelector ('.next');
+
+// Al click
+
+btnNext.addEventListener('click', function(){
+    console.log('click');
+    // Seleziono l'immagine attiva
+    const imagesActive = allImages[activeImage];
+
+    // Rimuovo l'active
+    imagesActive.classList.remove('active');
+
+    //Incremento
+    activeImage ++
+
+    //Selezione l'immagine successiva
+    const nextImg = allImages[activeImage];
+
+    nextImg.classList.add('active')
+})
